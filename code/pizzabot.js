65624c26fd4c09b2ba52fdd68dc98e0c80ac5b67
Nadia <63302901+Bluepoisson@users.noreply.github.com>
2020-09-08 12:14:27 +0200

    Sketch of Pizzabot
    Iteration 1,3 and 4 working.

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,7 +1,56 @@
-const vegetarian = "Vegetarian Pizza"
-const hawaiian = "Hawaiian Pizza"
-const pepperoni = "Pepperoni Pizza"
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = "Hawaiian Pizza";
+const pepperoni = "Pepperoni Pizza";
 
-const pizzaPrice = 80
+const pizzaPrice = 80;
 
 //Put your Javscript code here:
+
+console.log(vegetarian, hawaiian, pepperoni, pizzaPrice);
+
+alert(
+  `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
+);
+
+const orderName = prompt(
+  "Enter the name of the pizza you want to order today."
+);
+
+const checkOrderName = (orderName) => {
+  //   const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
+  if (
+    orderName === vegetarian ||
+    orderName === hawaiian ||
+    orderName === pepperoni
+  ) {
+    //     const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+    //   } else if (orderName === hawaiian) {
+    //     orderQuantity = prompt(`How many of ${orderName} do you want?`);
+    //   } else if (orderName === pepperoni) {
+
+    const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+  } else {
+    const wrongOrder = alert(`Sorry, we don't have that pizza on our menu.`);
+  }
+};
+
+checkOrderName();
+
+const orderTotal = orderQuantity * pizzaPrice;
+
+alert(
+  `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`
+);
+
+const cookingTime = () => {
+  if (orderQuantity < 2) {
+    prompt(`The pizzas will take 10 minutes.`);
+  } else if (orderQuantity > 2 && orderQuantity < 6) {
+    prompt(`The pizzas will take 15 minutes.`);
+  } else {
+    prompt(`The pizzas will take 20 minutes.`);
+  }
+};
+
+cookingTime();
