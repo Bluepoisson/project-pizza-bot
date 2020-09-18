298313e7fe3246c3d44e6c14d01d50f55cf2ac8c
Nadia <63302901+Bluepoisson@users.noreply.github.com>
2020-09-18 20:41:01 +0200

    Added functions and code 2 clean.
    Code 2 be cleaned.

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,23 +6,27 @@ const pizzaPrice = 80;
 
 //Put your Javscript code here:
 
-//test works
+//test
 //console.log(`Test if connection HTML-JS works: ${vegetarian}, ${hawaiian} and ${pepperoni}`);
 
 alert(
   `Welcome, customer! Happy to serve your pizza. On our menu we have: ${vegetarian}, ${hawaiian} and ${pepperoni}`
 );
 
-const orderName = prompt(
-  `Enter the name of the pizza you want to order today:`
-);
+// const orderName = prompt(`Enter the name of the pizza you want to order today:`);
+const orderName = (pizza) => {
+  pizza = prompt(`Enter the name of the pizza you want to order today:`);
+  return pizza;
+};
+orderName();
 
 //Function that tests whether the user input matches the pizza name
-const validateOrderName = (orderName) => {
+//pass orderName() function as a parameter?
+const validateOrderName = (orderName()) => {
   if (
-    orderName.toLowerCase() === vegetarian.toLowerCase() ||
-    orderName.toLowerCase() === hawaiian.toLowerCase() ||
-    orderName.toLowerCase() === pepperoni.toLowerCase()
+    orderName().toLowerCase() === vegetarian.toLowerCase() ||
+    orderName().toLowerCase() === hawaiian.toLowerCase() ||
+    orderName().toLowerCase() === pepperoni.toLowerCase()
   ) {
     return true;
   } else {
@@ -30,20 +34,27 @@ const validateOrderName = (orderName) => {
   }
 };
 
-var orderQuantity;
-var orderTotal;
+let orderQuantity;
+// var orderTotal;
+const calculateOrderTotal = (n1, n2) => {
+  result = n1 * n2;
+  return result;
+};
 
-if (validateOrderName(orderName)) {
-  orderQuantity = prompt(`How many of ${orderName}s do you want?`);
-  orderTotal = orderQuantity * pizzaPrice;
+if (validateOrderName(orderName())) {
+  orderQuantity = prompt(`How many of ${orderName()}s do you want?`);
   alert(
-    `Great, I'll get started on your ${orderName}s right away, it will cost ${orderTotal} kr.`
+    `Great, I'll get started on your ${orderName()}s right away, it will cost ${calculateOrderTotal(
+      orderQuantity,
+      pizzaPrice
+    )} kr.`
   );
 } else {
   alert(`Select a pizza from the menu.`);
+  // return orderName;
 }
 
-var cookingTime;
+let cookingTime;
 if (orderQuantity <= 2) {
   cookingTime = 10;
 } else if (orderQuantity >= 3 && orderQuantity < 6) {
@@ -52,5 +63,5 @@ if (orderQuantity <= 2) {
 
 document.getElementById(
   "orderSummary"
-).innerHTML = `Thank you for your order! The ${orderName}s are being
-baked as you read this. The total cost is ${orderTotal} kr. The order will take ${cookingTime} minutes.`;
+).innerHTML = `<h2>Thank you for your order!</h2> The ${orderName()}s are being
+prepared as you read this. The total cost is ${orderTotal} kr. The order will take ${cookingTime} minutes.`;
